3a05e68ace6db81f3f2a371ee7e62fdb4325955d
Jay Asbury <vbjay.net@gmail.com>
2020-04-06 22:28:51 -0400

    Add initial framework to provide file processing

---
+++ Image Processor/Image Processor/ImageFileProcessors/FileProcessor.vb
@@ -0,0 +1,19 @@
+﻿Namespace ImageFileProcessors
+    Public MustInherit Class FileProcessor(Of T)
+        Sub New(ByVal filePath As String)
+            Me.FilePath = filePath
+        End Sub
+
+        MustOverride Function Process() As T
+        ReadOnly Property FilePath As String
+
+    End Class
+
+    Public MustInherit Class ImageInfoRetriever
+        Inherits FileProcessor(Of ImageFileProcessInfo)
+
+        Sub New(ByVal filePath As String)
+            MyBase.New(filePath)
+        End Sub
+    End Class
+End Namespace
---
+++ Image Processor/Image Processor/ImageFileProcessors/ImageFileProcessInfo.vb
@@ -0,0 +1,12 @@
+﻿Namespace ImageFileProcessors
+    Public Class ImageFileProcessInfo
+        Property BitSize As Integer
+        Property ByteCount As Long
+        Property Bytes As Byte()
+        Property File As String
+        Property Format As String
+        Property PixelSize As Integer
+        Property Stride As Integer
+        Property Succeded As Boolean = True
+    End Class
+End Namespace
