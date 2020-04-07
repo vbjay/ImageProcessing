e601ef71ec522729168a5cc1a3eefe89611e68f4
Jay Asbury <vbjay.net@gmail.com>
2020-04-06 21:30:21 -0400

    Add generic ImageDataTask  to allow for task chaining
    The result of one task is the input of the next in the chain

---
+++ Image Processor/Image Processor/ImageDataTasks/ImageDataTask.vb
@@ -0,0 +1,79 @@
+﻿Namespace ImageDataTasks
+    Public MustInherit Class ImageDataTask
+        Protected MustOverride Iterator Function GenerateChildeSteps() As IEnumerable(Of ImageDataTask)
+
+        Overridable Sub Cleanup()
+
+        End Sub
+        MustOverride Async Function Start() As Task(Of ImageDataTaskInfo)
+    End Class
+
+    Public MustInherit Class ImageDataTask(Of TResult, TData)
+        Inherits ImageDataTask
+
+        Dim _data As TData
+
+        Sub New(Info As TData, Description As String, SourceFilePath As String)
+            TaskInfo = New ImageDataTaskInfo(Of TResult)(Description, SourceFilePath)
+            Data = Info
+
+        End Sub
+
+        Protected Overridable Async Function Process() As Task(Of TResult)
+            Dim result As TResult
+            result = Await Task.Run(Function()
+                                        Return Work(Data)
+                                    End Function)
+            Return result
+        End Function
+
+        Protected MustOverride ReadOnly Property Work As Func(Of TData, TResult)
+
+        Overrides Sub Cleanup()
+            MyBase.Cleanup()
+            If Data IsNot Nothing Then
+                If GetType(IDisposable).IsAssignableFrom(Data.GetType) Then DirectCast(Data, IDisposable).Dispose()
+                Data = Nothing
+            End If
+
+
+            If TaskInfo.Result IsNot Nothing Then
+                If GetType(IDisposable).IsAssignableFrom(TaskInfo.Result.GetType) Then DirectCast(TaskInfo.Result, IDisposable).Dispose()
+                TaskInfo.Result = Nothing
+            End If
+
+
+        End Sub
+        Overrides Async Function Start() As Task(Of ImageDataTaskInfo)
+            If TaskInfo.Status <> ImageDataTaskStatus.NotStarted Then Await Task.CompletedTask
+            Dim sw As New Stopwatch
+            sw.Start()
+            TaskInfo.Status = ImageDataTaskStatus.Running
+            Try
+                Dim result As TResult = Await Process()
+                TaskInfo.Result = result
+                TaskInfo.Status = ImageDataTaskStatus.Success
+                TaskInfo.ChildTasks = GenerateChildeSteps().ToArray
+            Catch ex As Exception
+                TaskInfo.Result = Nothing
+                TaskInfo.Status = ImageDataTaskStatus.Failed
+                TaskInfo.Description = $"{TaskInfo.Description}-{ex.Message}"
+                TaskInfo.ChildTasks = Enumerable.Empty(Of ImageDataTask)
+            Finally
+                sw.Stop()
+                TaskInfo.Time = sw.Elapsed
+            End Try
+            Return TaskInfo
+        End Function
+
+        Property Data As TData
+            Get
+                Return _data
+            End Get
+            Private Set
+                _data = Value
+            End Set
+        End Property
+        ReadOnly Property TaskInfo As ImageDataTaskInfo(Of TResult)
+    End Class
+End Namespace
---
+++ Image Processor/Image Processor/ImageDataTasks/ImageDataTaskInfo.vb
@@ -0,0 +1,27 @@
+﻿Namespace ImageDataTasks
+    Public Enum ImageDataTaskStatus
+        NotStarted = 0
+        Running
+        Success
+        Failed
+    End Enum
+    Public Class ImageDataTaskInfo
+        Property ChildTasks As IEnumerable(Of ImageDataTask)
+        Property Description As String
+        ReadOnly Property ID As Guid = Guid.NewGuid
+        Property SourceFilePath As String
+        Property Status As ImageDataTaskStatus
+        Property Time As TimeSpan = TimeSpan.Zero
+
+    End Class
+    Public Class ImageDataTaskInfo(Of T)
+        Inherits ImageDataTaskInfo
+
+        Sub New(ByVal description As String, SourceFilePath As String)
+            Me.Description = description
+            Me.SourceFilePath = SourceFilePath
+        End Sub
+
+        Property Result As T
+    End Class
+End Namespace
